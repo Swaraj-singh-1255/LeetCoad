f081a26e10c701596b5a947f8654b7562eb41341
Swaraj Singh <131450688+Swaraj-singh-1255@users.noreply.github.com>
2026-07-14 00:04:47 +0530

    Time: 44 ms (85.48%) | Memory: 54.8 MB (9.70%) - LeetSync

---
+++ 2749-promise-time-limit/promise-time-limit.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+    return async function(...args) {
+        // Create a promise that rejects automatically after t milliseconds
+        const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject("Time Limit Exceeded");
+            }, t);
+        });
+
+        // Race the actual function execution against our timeout promise
+        return Promise.race([
+            fn(...args),
+            timeoutPromise
+        ]);
+    };
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
